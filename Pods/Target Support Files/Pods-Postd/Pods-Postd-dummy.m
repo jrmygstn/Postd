6c9e1e95d6ffcc595cac76bdade8d590833a798f
Jeremy Gaston <jeremy@uballn.com>
2017-12-21 11:42:39 -0600

    Added files

---
+++ Pods/Target Support Files/Pods-Postd/Pods-Postd-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Postd : NSObject
+@end
+@implementation PodsDummy_Pods_Postd
+@end
---
+++ Pods/Target Support Files/Pods-PostdTests/Pods-PostdTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PostdTests : NSObject
+@end
+@implementation PodsDummy_Pods_PostdTests
+@end
